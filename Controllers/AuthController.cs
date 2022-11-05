d402b10989a3a835674789710f3dba8ae85a3075
Matheus Reis <matheus.reis081@gmail.com>
2022-11-05 12:45:29 -0300

    feat: created Post method to SignIn in AuthController

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,9 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
-namespace pim8.Controllers;
+using pim8.Models;
 
+namespace pim8.Controllers;
 public class AuthController : Controller {
 
     public IActionResult SignIn(){
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SignIn(AuthModel authModel){
+
+        Console.WriteLine(authModel.password);
+
+        if(authModel.username == "adm") {
+            Console.WriteLine("inside condition");
+            return RedirectToAction("Index", "Home");
+        }else {
+            ModelState.AddModelError("", "Inválido");
+        }
+
         return View();
     }
 }
