5449b7c69a10bc7b7e606cda7e02592bb9c1145b
Matheus Reis <matheus.reis081@gmail.com>
2022-11-05 17:25:07 -0300

    feat: signup success flow done

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -4,9 +4,15 @@ using Microsoft.AspNetCore;
 
 namespace pim8.Controllers;
 public class AuthController : Controller {
-
-    public IActionResult SignIn(){
+    private IActionResult IsLogged(){
+         string? loggedIn = Request.Cookies["SESSION_UNIP_PIM8"];
+        if(loggedIn != null) { 
+          return RedirectToAction("Index", "Home");
+        }
         return View();
+    }
+    public IActionResult SignIn(){
+        return IsLogged();
     }
 
     [HttpPost]
@@ -21,10 +27,26 @@ public class AuthController : Controller {
         return View();
     }
 
-     public IActionResult SignOut(){
+    public IActionResult SignOut(){
        Response.Cookies.Delete("SESSION_UNIP_PIM8");
        return RedirectToAction("SignIn", "Auth");
     }
-      
+     
+    public IActionResult SignUp(){
+        return View();
+    }
 
+    [HttpPost]
+    public IActionResult SignUp(UserModel userModel){
+        Console.WriteLine("Model" + ModelState.IsValid);
+        
+        if(ModelState.IsValid){
+        return RedirectToAction("SignUpSuccess", "Auth");
+        }
+        return View();
+    }
+
+    public IActionResult SignUpSuccess(){
+        return View();
+    }
 }
--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,9 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace pim8.Models {
 
     public class UserModel 
     {
-        public long id { get; set; }
+        
+        [Required(ErrorMessage = "Nome obrigatório")]
+        [Display(Name = "Nome Completo")]
         public string? name { get; set; }
+
+        [Required(ErrorMessage = "Nome de usuário obrigatório")]
+        [Display(Name = "Nome de Usuário")]
+        public string? username { get; set; }
+
+        [Required(ErrorMessage = "Senha obrigatória")]
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+
+        public string? password { get; set; }
+
+        [Required(ErrorMessage = "Confirmação de senha é obrigatória")]
+        [Display(Name = "Confirmação de senha")]
+        [DataType(DataType.Password)]
+
+        public string? confirmation_password { get; set; }
+        
+        [Required(ErrorMessage = "CPF obrigatório")]
+        [Display(Name = "CPF")]
+        public string? cpf { get; set; }
+
+        [Required(ErrorMessage = "Telefone obrigatório")]
+        [Display(Name = "Telefone")]
+        public string? phone { get; set; }
+
 
     }
 }
