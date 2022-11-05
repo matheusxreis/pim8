9ef246d69ca2af625f38ac47ba3babc5d0f190fb
Matheus Reis <matheus.reis081@gmail.com>
2022-11-05 12:44:29 -0300

    feat: created auth model to use in authentication form

---
+++ Models/AuthModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace pim8.Models {
+
+    public class AuthModel 
+    {
+        [Required(ErrorMessage = "Informe um nome de usuário.")]
+        [Display(Name = "Nome de usuário")]
+        public string? username { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string? password { get; set; }
+
+    }
+}
