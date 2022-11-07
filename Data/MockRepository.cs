e0af02845cfe15533197e9b446aaafbd84a57a53
Matheus Reis <matheus.reis081@gmail.com>
2022-11-07 14:27:29 -0300

    refactor: refactoring MockRepository  to use the database

--- Data/MockRepository.cs
+++ Data/MockRepository.cs
@@ -2,40 +2,43 @@
 namespace pim8.Data {
 
 
-    public class MockRepository {
-
-        static private MockRepository? INSTANCE;
-        private MockRepository(){}
-
-        static public MockRepository getInstance(){
-            if(MockRepository.INSTANCE == null){
-                MockRepository.INSTANCE = new MockRepository();
-            }
-            return MockRepository.INSTANCE;
+    public class MockRepository: iUserRepository {
+        public MockRepository(Context context){
+            _context = context;
         }
 
-        private List<UserEntity> users = new List<UserEntity>();
-
+        private readonly Context _context;
 
         public void save(UserEntity user){
-            this.users?.Add(user);
+           
+            _context.users.Add(user);
+            _context.SaveChanges();
            
         }
 
         public UserEntity? getUserByUsername(string? username){
-            UserEntity? u = users.Find(x=> x.username == username);
+
+            UserEntity? u = _context.users.FirstOrDefault(x=> x.username == username);
             return u;
 
         }
 
         public UserEntity? getUserById(string? id){
-            return users.Find(x=> x.id == id);
+            UserEntity? u = _context.users.FirstOrDefault(x=> x.id == id);
+             return u;
+
         }
 
 
         public void remove(string? email){
-            UserEntity? u = users.Find(x=>x.email == email);
-            if(u != null) { users.Remove(u); }
+            UserEntity? u = _context.users.FirstOrDefault(x=> x.email == email);
+            Console.WriteLine("++++++++++++++++++");
+            Console.WriteLine("Ooooooooi"+email+u?.email);
+            Console.WriteLine("++++++++++++++++++");
+
+            if(u != null) { _context.users.Remove(u); }
+            _context.SaveChanges();
+
         }
    
     }
