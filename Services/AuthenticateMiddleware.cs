af745c1b21ace2d07bacbd086646cc0e406da0fc
Matheus Reis <matheus.reis081@gmail.com>
2022-11-08 15:39:09 -0300

    feat: implement repository injection in AuthenticateMiddleware

--- Services/AuthenticateMiddleware.cs
+++ Services/AuthenticateMiddleware.cs
@@ -1,26 +1,40 @@
 using System.Globalization;
+using pim8.Models.Database;
+
 
 namespace pim8.Services
 {
     public class AuthenticateMiddleware
     {
         private readonly RequestDelegate _next;
-        private List<string> authUrls = new List<string> { "/Auth/SignUp", "/Auth/SignIn", "/Auth/SignUpSuccess" };  
+        private List<string> authUrls = new List<string> {"/", "/Auth/SignUp", "/Auth/SignIn", "/Auth/SignUpSuccess" };
         public AuthenticateMiddleware(RequestDelegate next)
         {
             _next = next;
+
         }
-        public async Task InvokeAsync(HttpContext context)
+        public async Task InvokeAsync(
+            HttpContext context,
+            iUserRepository userRepository
+            )
         {
             string? authCookie = context.Request.Cookies["SESSION_UNIP_PIM8"];
+
+            string path = context.Request.Path;
+            Boolean isAuthUrl = authUrls.Any(x => x == path);
+            if (isAuthUrl)
+            {
+                await _next(context);
+                return;
+            }
             if (authCookie == null)
             {
-                string path = context.Request.Path;
-                if(authUrls.Any(x=>x == path) == false) { 
-                context.Response.Redirect("/Auth/SignIn");
-                return;
-                }
+                context.Response.Redirect("/Auth/SignIn"); return;
+            } else {
+                UserModel? user = userRepository.getUserById(authCookie);
+                if(user?.name == null) { context.Response.Redirect("/Auth/SignIn"); return; }
             }
+
             await _next(context);
         }
     }
