4abfef7c57fb51a63a243738089dbe8c806989f5
Matheus Reis <matheus.reis081@gmail.com>
2022-11-07 21:37:45 -0300

    feat: created migration to add confirmationToken and active columns in users table

---
+++ Models/Database/Migrations/20221108003035_ConfirmationTokenColumn.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pim8.Models.Database.Migrations
+{
+    public partial class ConfirmationTokenColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "confirmation_token",
+                table: "users",
+                nullable: true
+            );
+            migrationBuilder.AddColumn<Boolean>(
+                name: "active",
+                table: "users",
+                defaultValue: false
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "confirmation_token",
+                table: "users"
+            );
+             migrationBuilder.DropColumn(
+                name: "active",
+                table: "users"
+            );
+        }
+    }
+}
