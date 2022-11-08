d1b541d6fb84ee38edc5ad4bbd6f525805cec857
Matheus Reis <matheus.reis081@gmail.com>
2022-11-07 21:50:29 -0300

    feat: created implementation from GenerateToken

---
+++ Helpers/GenerateToken.cs
@@ -0,0 +1,11 @@
+using pim8.Controllers.iHelpers;
+
+namespace pim8.Helpers
+{
+    public class GenerateToken:iGenerateEmailToken {
+        
+       public string generate(){
+            return "aas";
+        }
+    }
+}
