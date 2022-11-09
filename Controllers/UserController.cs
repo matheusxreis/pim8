ce5e3e09a33a11df75cd8367a5c6c666a77ae2c6
Matheus Reis <matheus.reis081@gmail.com>
2022-11-09 12:35:23 -0300

    feat: implement updatePhoto action with repository injection in UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,26 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using pim8.Models.Database;
 
 
 namespace pim8.Controllers
 {
-    public class UserController: Controller
+    public class UserController : Controller
     {
-        public IActionResult UpdatePhoto(string files){
-            if(files != null) { 
-                // var byteArrayFile = System.fILE
-                // var x = Convert.ToBase64String();
 
-                var filePath = Path.GetTempFileName();
-                byte[] byteFile = System.IO.File.ReadAllBytes(filePath);
-                string base64 = Convert.ToBase64String(byteFile);
 
-                //var stream = System.IO.File.Create(filePath);
-                //files[0].CopyToAsync(stream);
-                Console.WriteLine($"toptop, file veio{files.ToString()}");
-            }else {
-                Console.WriteLine($"TRISTEZA TRISTEZA!");
+        private readonly iUserRepository _userRepository;
 
-            }
+        public UserController(iUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePhoto(string file)
+        {
+
+            if (file != null)
+            {
+                string? id = Request.Cookies["SESSION_UNIP_PIM8"];
+                _userRepository.updatePhoto(file, id ?? "");           
+                
+             }
+            
 
             return RedirectToAction("Profile", "Home");
         }
