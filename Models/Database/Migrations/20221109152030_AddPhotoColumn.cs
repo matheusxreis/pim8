78bcc08dc7b8c342559ece0e7d1c398016a71020
Matheus Reis <matheus.reis081@gmail.com>
2022-11-09 12:26:25 -0300

    feat: created migration to photo column

---
+++ Models/Database/Migrations/20221109152030_AddPhotoColumn.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pim8.Migrations
+{
+    public partial class AddPhotoColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+            migrationBuilder.AddColumn<string>(
+                name: "photo",
+                table: "users",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+            migrationBuilder.DropColumn(
+                name: "photo",
+                table: "users");
+        }
+    }
+}
