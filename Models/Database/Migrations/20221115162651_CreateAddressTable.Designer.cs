0234ee1fcc7f29db9b300327480430bac482b474
Matheus Reis <matheus.reis081@gmail.com>
2022-11-15 13:28:35 -0300

    feat: create AddressTable migration

--- Models/Database/Migrations/20221115162651_CreateAddressTable.Designer.cs
+++ Models/Database/Migrations/20221115162651_CreateAddressTable.Designer.cs
@@ -2,18 +2,20 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using pim8.Models.Database;
 
 #nullable disable
 
-namespace pim8.Migrations
+namespace pim8.Models.Database.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20221115162651_CreateAddressTable")]
+    partial class CreateAddressTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Models/Database/Migrations/20221115162651_CreateAddressTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pim8.Models.Database.Migrations
+{
+    public partial class CreateAddressTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "address",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    place = table.Column<string>(type: "text", nullable: false),
+                    number = table.Column<string>(type: "text", nullable: false),
+                    cep = table.Column<string>(type: "text", nullable: false),
+                    neighborhood = table.Column<string>(type: "text", nullable: false),
+                    city = table.Column<string>(type: "text", nullable: false),
+                    state = table.Column<string>(type: "text", nullable: false),
+                    complement = table.Column<string>(type: "text", nullable: true),
+                    user = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_address", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "address");
+        }
+    }
+}
