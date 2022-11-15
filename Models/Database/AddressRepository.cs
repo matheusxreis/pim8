677c001b9b736057d6aa8c2df9e195a7c0559d34
Matheus Reis <matheus.reis081@gmail.com>
2022-11-15 16:23:49 -0300

    refactor: updating address case already exist

--- Models/Database/AddressRepository.cs
+++ Models/Database/AddressRepository.cs
@@ -11,7 +11,24 @@ namespace pim8.Models.Database {
         private readonly Context _context;
 
         public void save(AddressModel address) {
+
+            AddressModel? alreadyExist = _context.address.FirstOrDefault(x=> x.user == address.user);
+            Console.WriteLine(alreadyExist?.ToString());
+            if(alreadyExist == null){
             _context.address.Add(address);
+            _context.SaveChanges();
+            return;
+            }
+
+             alreadyExist.cep = address.cep;
+             alreadyExist.city = address.city;
+             alreadyExist.state = address.state;
+             alreadyExist.neighborhood = address.neighborhood;
+             alreadyExist.complement = address.complement;
+             alreadyExist.number = address.number;
+             alreadyExist.place = address.place;
+
+             _context.address.Update(alreadyExist);
             _context.SaveChanges();
         }
 
